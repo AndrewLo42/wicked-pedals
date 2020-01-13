a4eb05d94bb15e1ff5428d22901a0369ade91232
andrewlo42 <andrewclo42@gmail.com>
2020-01-13 23:56:03 +0000

    css changes

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div className="p-3 bg-dark">
+      <div className="p-3 bg-dark header">
         <i className="d-inline fas fa-dollar-sign text-white pt-2 pl-5">
           <div className="d-inline"> Wicked Sales </div>
         </i>
