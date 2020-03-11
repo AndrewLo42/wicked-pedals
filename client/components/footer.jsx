9fe63351db70b25e47436c4bd0e00ff3256e38aa
andrewlo42 <andrewclo42@gmail.com>
2020-03-11 22:09:25 +0000

    completed footer component

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -4,8 +4,17 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="p-3 bg-dark footer">
-        <div className="container-fluid">
-          Yes
+        <div className="footer-container container-fluid">
+          <div className="d-flex row justify-content-center">
+            <a
+              className="footer-link row"
+              href="https://andrewclo.com"
+              target="_blank"
+              rel="noopener noreferrer">
+              <img className=" footer-logo" src="./images/Andrew-Lo-go.png"></img>
+              <div className="pt-2 footer-creds"> Andrew Lo</div>
+            </a>
+          </div>
         </div>
       </div>
     );
